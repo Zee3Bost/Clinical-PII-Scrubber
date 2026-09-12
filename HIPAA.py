a05acecb6bf401236acc23f6a2b48c5102cf2b90
Zee3Bost <zion.bost@gmail.com>
2026-09-12 16:46:36 -0400

    Add files via upload

--- HIPAA.py
+++ HIPAA.py
@@ -1,8 +1,7 @@
-clinical_note = "Patient Symere Woods called 555-0199 today. Symere reported fever and gave SSN 123-45-6789."
-#clinical_note2 = "Patient Victoria Walker called 888-1400 today. Victoria reported abdomen pain and gave SSN 987-65-4321."
+clinical_note = input("Enter the clinical note to scrub: ")
 
 real_names = ["Symere", "Woods", "Victoria", "Walker"]
-fake_names = ["Alex", "Taylor", "Jordan", "Doe"]
+fake_names = ["####"]
 
 words = clinical_note.split()
 scrubbed_words = []
@@ -10,7 +9,7 @@ scrubbed_words = []
 for word in words:
     clean_word = word.strip(".,!?")
     if clean_word in real_names:
-        scrubbed_words.append("Alex")
+        scrubbed_words.append("####")
     elif len(clean_word) == 11 and clean_word[3] == "-" and clean_word[6] == "-":
         scrubbed_words.append("[REDACTED_SSN]")
     elif len(clean_word) == 8 and clean_word[3] == "-":
